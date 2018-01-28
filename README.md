a65086730150e40f7d967372b252544a52d9fb30
Jonas Stumph Stevnsvig <jonas@stevnsvig.com>
2018-01-28 16:55:48 +0100

    added drawing

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # OpenGLTest
 OpenGL experiment
+
+
+## Inspiration
+
+https://solarianprogrammer.com/2014/12/08/getting-started-jogl-java-opengl-eclipse/
+https://solarianprogrammer.com/2013/05/13/opengl-101-drawing-primitives/
+https://www.tutorialspoint.com/jogl/jogl_drawing_basics.htm
+https://www.tutorialspoint.com/jogl/jogl_quick_guide.htm
+http://jogamp.org/jogl/www/
+
+## Javadoc
+
+https://jogamp.org/deployment/v2.3.2/javadoc/jogl/javadoc/
--- src/main/java/dk/stumph/opengltest/Game.java
+++ src/main/java/dk/stumph/opengltest/Game.java
@@ -3,6 +3,7 @@ package dk.stumph.opengltest;
 import javax.swing.JFrame;
 
 import com.jogamp.opengl.GL4;
+import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.GLAutoDrawable;
 import com.jogamp.opengl.GLCapabilities;
 import com.jogamp.opengl.GLEventListener;
@@ -17,15 +18,13 @@ public class Game extends JFrame implements GLEventListener {
 
 	public Game() {
 		super(TITLE);
-		
-		
+
 		GLProfile profile = GLProfile.get(GLProfile.GL2);
 		GLCapabilities capabilities = new GLCapabilities(profile);
-		
+
 		GLCanvas canvas = new GLCanvas(capabilities);
 		canvas.addGLEventListener(this);
 
-		
 		this.getContentPane().add(canvas);
 		this.setName(TITLE);
 		this.setSize(WIDTH, HEIGHT);
@@ -34,15 +33,29 @@ public class Game extends JFrame implements GLEventListener {
 		this.setVisible(true);
 		this.setResizable(false);
 		canvas.requestFocusInWindow();
-		
+
 	}
 
 	public void display(GLAutoDrawable drawable) {
-		GL4 gl = drawable.getGL().getGL4();
-		gl.glClear(GL4.GL_COLOR_BUFFER_BIT | GL4.GL_DEPTH_BUFFER_BIT);
-		
-		//TODO: call draw code here
-		
+		GL2 gl = drawable.getGL().getGL2();
+		gl.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
+
+		// TODO: call draw code here
+		gl.glBegin(GL2.GL_LINE_LOOP);
+		gl.glVertex3f(-0.50f, -0.75f, 0);
+		gl.glVertex3f(0.7f, 0.5f, 0);
+		gl.glVertex3f(0.70f, -0.70f, 0);
+		gl.glVertex3f(0f, 0.5f, 0);
+
+		gl.glEnd();
+
+		gl.glBegin(GL2.GL_TRIANGLES); // Drawing Using Triangles
+
+		gl.glVertex3f(0.1f, 0.1f, 0.0f); // Top
+		gl.glVertex3f(-0.2f, -0.10f, 0.0f); // Bottom Left
+		gl.glVertex3f(0.1f, -0.5f, 0.0f); // Bottom Right
+		gl.glEnd();
+
 		gl.glFlush();
 	}
 
@@ -57,9 +70,29 @@ public class Game extends JFrame implements GLEventListener {
 
 	}
 
-	public void reshape(GLAutoDrawable drawable, int arg1, int arg2, int arg3, int arg4) {
-		// TODO Auto-generated method stub
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
 
+		// TODO Auto-generated method stub
+		final GL2 gl = drawable.getGL().getGL2();
+
+		// get the OpenGL 2 graphics object
+		if (height <= 0)
+			height = 1;
+
+		// preventing divided by 0 exception height = 1;
+		final float h = (float) width / (float) height;
+
+		// display area to cover the entire window
+		gl.glViewport(0, 0, width, height);
+
+		// transforming projection matrix
+		gl.glMatrixMode(GL2.GL_PROJECTION);
+		gl.glLoadIdentity();
+		// gl.gluPerspective(45.0f, h, 1.0, 20.0);
+
+		// transforming model view gl.glLoadIdentity();
+		gl.glMatrixMode(GL2.GL_MODELVIEW);
+		gl.glLoadIdentity();
 	}
 
 	public void play() {
